94e99226d927dddb0788944d1606ba8e5d0f2c66
Christian Vera <Crvc1998@gmail.com>
2022-04-01 22:00:02 -0500

    Move routers, setup settings and urls

--- CoreRoot/urls.py
+++ CoreRoot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("discount.routers"), name="core-api")
+    path('api/', include("CoreRoot.routers"), name="cart-api"),
 ]
