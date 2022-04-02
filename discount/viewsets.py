2e9a595601d46711d0bc7186ecbb0a383a86c4ff
Christian Vera <Crvc1998@gmail.com>
2022-04-01 21:16:08 -0500

    Create test_viewsets, add routers and urls

---
+++ discount/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from discount.models import Discount
+from discount.serializers import DiscountSerializer
+
+
+class DiscountViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post']
+    serializer_class = DiscountSerializer
+    permission_classes = (AllowAny,)
+    queryset = Discount.objects.all()
